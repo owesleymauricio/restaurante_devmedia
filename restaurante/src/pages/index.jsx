2d185baf1a2795799f545657f35b04ab8010d333
owesleymauricio <wesleymauricio85@gmail.com>
2023-10-26 12:28:37 -0300

    imagens quebrando

---
+++ restaurante/src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Banner from '../assets/banner.png'
+
+export default function Home() {
+  return (
+    
+     <div>
+      <header className='banner'>
+      <img src={Banner} alt="banner" />
+      </header>
+     
+     </div>
+    
+  )
+}
